644c747a6d7f6e649aab090a9f7b96fbb1c31857
KreativStudio <oreskoegor@gmail.com>
2025-08-28 19:40:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using System.Runtime.Versioning;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Documents;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        for (int i = 1; i < 101; i++)
+        {
+            if ((i % 3 == 0) && (i % 5 == 0))
+                Console.WriteLine("FizzBuzz");
+            else if (i % 3 == 0)
+                Console.WriteLine("Fizz");
+            else if (i % 5 == 0)
+                Console.WriteLine("Buzz");
+            else
+                Console.WriteLine(i);
+        }
+
+
+
+
+
+
+    }       
+}
+
